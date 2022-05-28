93c5a1fc4b56a086c6258cb5db2124e0fa78790e
Davi Vilarinho <davi.vilarinho@exacode.com.br>
2022-05-28 09:59:37 -0300

    basic gitea and terraform

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.15.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "vilarinho-state"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
+
+  default_tags {
+    tags = {
+      Application = "gitea"
+      Group = "terraform-managed"
+    }
+  }
+}
